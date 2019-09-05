5401c3f51969859ef05abf7e11eb0863fb469ebe
Juan Chango <jchango@udlanet.ec>
2019-09-05 17:24:10 -0400

    format working!

--- prepros.py
+++ prepros.py
@@ -1,43 +1,23 @@
+
 import re
 import pandas as pd
 import numpy as np
 import keras
 
 
-def clean_review(text):
+def clean_review(text_arg):
     # Strip HTML tags
-    text = re.sub('<[^<]+?>', ' ', text)
-
-    # Remove special characters
-    text = re.sub('[^a-zA-Z0-9 \n\.]', '', text)
-
-    # Replace numbers
-    text = re.sub(r'[0-9]+', 'a_number', text)
-
-    # Strip escaped quotes
-    text = text.replace('\\"', '')
-
-    # Strip quotes
-    text = text.replace('"', '')
-
-    return text
+    # text = re.sub('<[^<]+?>', ' ', text)
+    text_arg = re.sub("[!@#$%^&*()[]{};:,./<>?\|`~-=_+]", " ", text_arg)
+    return text_arg
 
 
 def has_numbers(input_string):
     return bool(re.search(r'\d', input_string))
 
 
-input_path = "/Users/Juan/Downloads/mercadolibre_data/train.csv"
-data = pd.read_csv(input_path, nrows=100)
-
-data['title_cleaned'] = data['title'].apply(clean_review)
-
-
-
-text = data['title'][0]
-
-def set_custom_format(text):
-    words = text.split()
+def custom_text_format(text_arg):
+    words = text_arg.split()
     text_fixed = ''
 
     for w in words:
@@ -50,8 +30,26 @@ def set_custom_format(text):
                 text_fixed += 'a_number'
             except:
                 text_fixed += 'a_mixed_number'
-                print('it has mixed words')
+                # print('it has mixed words')
         else:
             text_fixed += w
 
         text_fixed += ' '
+
+    text_fixed = re.sub("[!@#$%^&*()[]{};:,./<>?\|`~-=_+]", " ", text_fixed)
+
+    return text_fixed.lower()
+
+
+
+
+input_path = "/Users/Juan/Downloads/mercadolibre_data/train.csv"
+data = pd.read_csv(input_path, nrows=100)
+
+data['title_cleaned'] = data['title'].apply(custom_text_format)
+
+#
+#
+# text = data['title'][0]
+#
+# data['title_cleaned'][80]
