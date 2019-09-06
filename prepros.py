57e32b8127d36d110546c2825e5669802b90bfbb
Juan Chango <jchango@udlanet.ec>
2019-09-06 12:05:06 -0400

    added MLP model

--- prepros.py
+++ prepros.py
@@ -11,6 +11,12 @@ from keras.preprocessing.text import text_to_word_sequence
 from keras.preprocessing.text import Tokenizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import LabelBinarizer
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import Dense, Conv1D, MaxPooling1D, Dropout
+from keras.optimizers import *
+from keras.losses import *
+from keras.activations import *
 
 def has_numbers(input_string):
     return bool(re.search(r'\d', input_string))
@@ -85,42 +91,70 @@ lb_style = LabelBinarizer()
 lb_results = lb_style.fit_transform(data["category"])
 
 
+print(encoded_docs.shape)
+print(lb_results.shape)
 
 
 
-
-X_train, X_test, y_train, y_test = train_test_split(data['title_cleaned'], data['category'], test_size=0.2)
-
-
+Y_pre = lb_results
+X_pre = encoded_docs
 
 
+x_train, x_val, y_train, y_val = train_test_split(X_pre, Y_pre, test_size=0.3)
 
 
-vectorizer = CountVectorizer(binary=True, stop_words=stopwords.words('english'),
-                             lowercase=True, min_df=3, max_df=0.9, max_features=5000)
+model = Sequential()
+model.add(Dense(1024, activation='relu', input_dim=x_train.shape[1]))
+model.add(Dropout(0.5))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(y_train.shape[1], activation='sigmoid'))
 
+model.compile(loss=binary_crossentropy, optimizer='adam', metrics=['accuracy'])
 
-X_train_onehot = vectorizer.fit_transform(X_train)
-
-
-
-# define the document
-text = 'The quick brown fox jumped over the lazy dog.'
-# tokenize the document
-result = text_to_word_sequence(text)
-print(result)
-
-
+model.fit(x_train, y_train,
+                     validation_data=[x_val, y_val],
+                     epochs=32,
+                     batch_size=32,
+                     verbose=2)
 
 
 
 #
-# text = data['title'][0]
-#
-# result = text.translate(str.maketrans('', '', string.punctuation))
-
 #
 #
-# text = data['title'][0]
+# X_train, X_test, y_train, y_test = train_test_split(data['title_cleaned'], data['category'], test_size=0.2)
 #
-# data['title_cleaned'][80]
+#
+#
+#
+#
+#
+# vectorizer = CountVectorizer(binary=True, stop_words=stopwords.words('english'),
+#                              lowercase=True, min_df=3, max_df=0.9, max_features=5000)
+#
+#
+# X_train_onehot = vectorizer.fit_transform(X_train)
+#
+#
+#
+# # define the document
+# text = 'The quick brown fox jumped over the lazy dog.'
+# # tokenize the document
+# result = text_to_word_sequence(text)
+# print(result)
+#
+#
+#
+#
+#
+# #
+# # text = data['title'][0]
+# #
+# # result = text.translate(str.maketrans('', '', string.punctuation))
+#
+# #
+# #
+# # text = data['title'][0]
+# #
+# # data['title_cleaned'][80]
