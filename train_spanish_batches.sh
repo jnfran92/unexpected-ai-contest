819b283365ac2bf3c8f4652d33c3ddeaddfdbc63
Juan Chango <jchango@udlanet.ec>
2019-09-30 20:51:42 -0300

    reinforcement learning data added and ready for spanish!

--- train_spanish_batches.sh
+++ train_spanish_batches.sh
@@ -4,6 +4,9 @@
 
 source /home/juan_francisco_chango/envs/tf-gpu/bin/activate
 
+python3 9_train_model_rich_cnn_lstm_spanish.py
+python3 10_continue_train_model_rich_cnn_lstm_spanish.py 1 0.0001
+python3 10_continue_train_model_rich_cnn_lstm_spanish.py 2 0.0001
+python3 10_continue_train_model_rich_cnn_lstm_spanish.py 3 0.0001
 python3 10_continue_train_model_rich_cnn_lstm_spanish.py 4 0.0001
 python3 10_continue_train_model_rich_cnn_lstm_spanish.py 5 0.0001
-python3 10_continue_train_model_rich_cnn_lstm_spanish.py 6 0.0001
