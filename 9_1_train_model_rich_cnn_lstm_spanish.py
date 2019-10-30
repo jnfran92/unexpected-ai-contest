29c5de5008722ec8dcfc98899f61e2213bfa9c9b
Juan Francisco Chango <jchango@udlanet.ec>
2019-10-30 20:17:31 -0300

    reinforcement learning data added and ready for spanish!

--- 9_1_train_model_rich_cnn_lstm_spanish.py
+++ 9_1_train_model_rich_cnn_lstm_spanish.py
@@ -94,7 +94,7 @@ csv_logger = CSVLogger(filename="./logs/" + model_name + str(n_batch) + ".csv")
 fit_data = model.fit(x_train, y_train,
                      validation_data=[x_val, y_val],
                      epochs=2,
-                     batch_size=1024,
+                     batch_size=2048,
                      verbose=2,
                      callbacks=[early_stop, csv_logger])
 
