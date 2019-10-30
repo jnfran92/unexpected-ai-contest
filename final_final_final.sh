8eba69ba4c148acad1b4cfc4c02aa0db708e364b
Juan Francisco Chango <jchango@udlanet.ec>
2019-10-30 18:05:58 -0300

    reinforcement learning data added and ready for spanish!

--- final_final_final.sh
+++ final_final_final.sh
@@ -3,7 +3,9 @@
 # nohup bash ./final_final_final.sh > final_final_final_log_3.out &
 
 source /home/juan_francisco_chango/envs/tf-gpu/bin/activate
+python3 9_1_train_model_rich_cnn_lstm_spanish.py
+#python3 12_1_continue_train_model_rich_cnn_lstm_portuguese.py 0 6 7 0.0001
+#python3 10_1_continue_train_model_rich_cnn_lstm_spanish.py 0 8 9 0.0001
 
-python3 12_1_continue_train_model_rich_cnn_lstm_portuguese.py 0 6 7 0.0001
-python3 10_1_continue_train_model_rich_cnn_lstm_spanish.py 0 8 9 0.0001
+
 
